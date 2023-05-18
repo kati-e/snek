3a1dc05f475d2e068a5b1b4c115c1e70988a4aac
Katie <clearykatelyn@gmail.com>
2023-05-18 20:11:45 +1000

    neatened up ending screen, made a beat high score variable

--- Game.cs
+++ Game.cs
@@ -10,34 +10,157 @@ public class Game
         return gameover;
     }
 
-    public void End(int score)
+    public void Start()
     {
-        int Score = score;
-        //int HighScore = highScore;
-
+        snekBanner();
         WriteLine(
-            @"                     ___" + "\n" +
-            @"                    / o o\ " + "\n" +
-            @"                    \   --<    Sssss.s.s.s.ss..." + "\n" +
-            @"                     | |" + "\n" +
-            @"         ____________/ /" + "\n" +
-            @"        /  ___________/" + "\n" +
-            @"       |  |               ___     _   __" + "\n" +
-            @"        \ _\_   _      _ / __\___| | / /" + "\n" +
-            @"         / __| | |    | | /  ____| |/ /  ___" + "\n" +
-            @"   Sss.s.. \_\_|  \  /| ||  /__\_|   /  / _ \__" + "\n" +
-            @"          \___ \ \ \/ | ||   ____|   \ / / \____\ " + "\n" +
-            @"           __/ / |\ \/| ||  |____| |\ \ / " + "\n" +
-            @"          |___/|_| \____/ \______|_|\\_\    Sssss .ss.s." + "\n" +
-            @"               \___/              \___/" + "\n" +
-            @"                                                        " + "\n" +
-            @"          THANK YOU FOR PLAYING SNAKE (snek)!" + "\n" +
-            @"          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n" +
-            @"          Your Score: " + Score + "\n" +
-            @"          High Score: " + "HighScore" + "\n"
-                        //Need to add in the variable for high score later
+            "                    Press 'N' to Start!    \n\n"
             );
 
+        PrintRules();
+
+        Write("\n\n >> ");
+        string Input = ReadLine();
+        bool Valid = ValidInput(Input);
+
+        while (!Valid)
+        {
+            Write("\n\n Invalid. Please Type 'N' >> ");
+            Input = ReadLine();
+            Valid = ValidInput(Input);
+        }
+
+    }
+    public bool ValidInput(string input)
+    {
+        bool valid;
+
+        if(input != "N")
+        {
+            valid = false;
+        }
+        else
+        {
+            valid = true;
+        }
+
+        return valid;
+    }
+    public void PrintRules()
+    {
+        string[] Rules = new string[5] {
+            "Snake moves right, left, up & down (A,D,W,S)",
+            "To gain points you must eat fruits (#)",
+            "When you eat fruits you gain a body part",
+            "You die if your body touches itself or the wall",
+            "Your 'High Score' is saved in history" };
+
+        WriteLine(" Rules: 1. " + Rules[0]);
+
+        for (int i = 1; i < Rules.Length; i++)
+        {
+            WriteLine("        " + (i + 1) + ". " + Rules[i]);
+        }
+
+        WriteLine("\n\n Created by: kati-e");
+    }
+
+    public bool End(int score, int highScore)
+    {
+        int Score = score;
+        int HighScore = highScore;
+        bool beat = false;
+
+        Clear();
+        WriteLine(
+            @"___________________________________________________________" + "\n" +
+            @"|                                                         |" + "\n" +
+            @"|                                                         |" + "\n" +
+            @"|                     ___                                 |" + "\n" +
+            @"|                    / x x\                               |" + "\n" +
+            @"|                    \   --<    Sssss.s.s.s.ss...         |" + "\n" +
+            @"|                     | |                                 |" + "\n" +
+            @"|         ____________/ /                                 |" + "\n" +
+            @"|        /  ___________/                                  |" + "\n" +
+            @"|       |  |               ___     _   __                 |" + "\n" +
+            @"|        \ _\_   _      _ / __\___| | / /                 |" + "\n" +
+            @"|         / __| | |    | | /  ____| |/ /  ___             |" + "\n" +
+            @"|   Sss.s.. \_\_|  \  /| ||  /__\_|   /  / _ \__          |" + "\n" +
+            @"|          \___ \ \ \/ | ||   ____|   \ / / \____\        |" + "\n" +
+            @"|           __/ / |\ \/| ||  |____| |\ \ /                |" + "\n" +
+            @"|          |___/|_| \____/ \______|_|\\_\    Sssss .ss.s. |" + "\n" +
+            @"|               \___/              \___/                  |" + "\n" +
+            @"|                                                         |" + "\n" +
+            @"|          THANK YOU FOR PLAYING SNEK!!                   |" + "\n" +
+            @"|          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~                   |"
+            );
+
+
+        //Stringing the score and high score to appear in the end screen
+        string printScore = Convert.ToString(score);
+        string printHighScore = Convert.ToString(highScore);
+
+        //What is the point of making them into strings?
+        //I need to add or take spaces depending on the number of digits
+        //of the score to make it fit nicely in the end screen graphic
+        //(It was designed to fit 3-digits, anything else needs adjusting)
+
+        //Is the score two digits? Add one space.
+        if (score < 100 && score > 9)
+        {
+            printScore += " ";
+        }
+        if (highScore < 100 && highScore > 9)
+        {
+            printHighScore += " ";
+        }
+
+        //Is the scores a single digit? Add two spaces.
+        if (score < 10)
+        {
+            printScore += "  ";
+        }
+        if (highScore < 10)
+        {
+            printHighScore += "  ";
+        }
+
+        //having two different bases to the end screen below
+        //depending on beating highscore or not
+        if (score > highScore)
+        {
+
+            WriteLine(
+                @"|          Your score: " + printScore + " ~~ NEW HIGH SCORE ~~           |" + "\n" +
+                @"|          Highscore: " + printHighScore + "                                 |" + "\n" +
+                @"|                                                         |" + "\n" +
+                @"|          CONGRATULATIONS!! You beat the highscore!      |" + "\n" +
+                @"|_________________________________________________________|"
+                );
+            beat = true;
+        } else
+        {
+            WriteLine(
+                @"|          Your score: " + printScore + "                                |" + "\n" +
+                @"|          Highscore: " + printHighScore + "                                 |" + "\n" +
+                @"|                                                         |" + "\n" +
+                @"|_________________________________________________________|"
+                );
+            beat = false;
+        }
+
+        return beat;
+
+    }
+
+    public static void snekBanner()
+    {
+        Clear();
+        WriteLine(
+            "|----------------------------------------------------------|\n" +
+            "|------------------------ SNEK GAME! ----------------------|\n" +
+            "|----------------------------------------------------------|\n\n"
+            );
     }
 
 } 
--- Program.cs
+++ Program.cs
@@ -88,23 +88,42 @@ class Program
     {
         //Setup Game
         Game snake = new Game();
-        int score = 0;
+        int score = 999;
+        int highscore = 99;
         bool endGame = snake.Setup();
 
+        //Start Game
+        snake.Start();
+        score = 100; // initialise starting score at 0
+
+        //Play Game
         while (endGame == false)
         {
             //Draw Board
 
-
             //Input
-
+            // A W D or S
 
             //Logic
 
+            //if eats fruit then score++
+
+            //if hits wall then endGame = true
+
+            //if hits self then endGame = true
+            endGame = true;
+
         }
 
-        //End
+        //End Game
         endGame = true;
-        snake.End(score);
+        //function to compare end score to current highscore
+        
+        bool beatHighScore = snake.End(score, highscore);
+
+        if (beatHighScore)
+        {
+            //if score > highscore --> highscore == score (and then save in storage)
+        }
     }
 }
